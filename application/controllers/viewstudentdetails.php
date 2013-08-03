b4bd939ead9fb8641bdf5b0db85a7912badec608
Adelen Victoria Po Festin <festinadelen@gmail.com>
2013-08-03 16:33:44 +0800

    LOL

---
+++ application/controllers/viewstudentdetails.php
@@ -0,0 +1,36 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Viewstudentdetails extends Main_Controller {	
+
+	public function __construct() {
+		parent::__construct(true);
+		$this->load->model('Viewstudentdetails_Model', 'Model');
+	}
+	
+   public function index()
+	{
+		$this->loadstudentinfo();
+	}
+	
+	public function loadstudentinfo() {
+		$studentid = $this->input->post('studentid');
+		$studentid = 1;
+		$studentinfo = $this->Model->getstudentinfo($studentid);
+		//print_r($studentinfo);
+		//die();
+		$this->load_view('viewstudentdetails_view', compact('studentinfo'));
+	}
+	
+	public function downloadfile() {
+	$filename = $this->input->post('filename');
+	$filepath =  $this->input->post('filepath');
+	//download.php
+	//content type
+	header('Content-type: application/pdf');
+	//open/save dialog box
+	header('Content-Disposition: attachment; filename=iskolarship.pdf');
+	//read from server and write to buffer
+	readfile($filepath);
+	}
+	
+}
---
+++ application/views/viewstudentdetails_view.php
@@ -0,0 +1,25 @@
+ 
+   <?$fullname = array($studentinfo['firstname'], $studentinfo['middlename'], $studentinfo['lastname'], $studentinfo['namesuffix']);?>
+
+   	<div class="row">   
+		<div class="well" align="center">
+			<span>
+				<img src="<?= base_url('images/pic.png')?>" style="float:left; padding-left:20px;"/>
+			</span>
+			<span>
+			Name: <?=implode($fullname, " ")?><br/>
+			Sex: <?=($studentinfo['sex'] == 1 ? 'Female' : 'Male')?><br/>
+			Birthday: <?=$studentinfo['birthday']?><br/>
+			Degree Program: <?=$studentinfo['name']?><br/>
+			Family Income: <?=$studentinfo['familyincome']?><br/>
+			Reason For Needing Scholarship: <?=$studentinfo['reasonforneedingscholarship']?><br/>
+			Target Money: <?=$studentinfo['targetmoney']?><br/>
+			<form method="post" action="<?= base_url('viewstudentdetails/downloadfile')?>">
+				<input type = "hidden" name = "filename" value = "sample.pdf"/>
+				<input type = "hidden" name = "filepath" value = "C:\Users\adelin\Desktop\pandesal.pdf"/>
+				<input type="submit" value = "Download"/>
+			</form>
+			</span>
+		</div>
+	</div>			
+</div>
