51dd78be4186159689bfe190892415bbc58243e9
danantonioreyes <danantonioreyes@gmail.com>
2013-08-28 18:46:35 +0800

    sponsorhomepage_model

--- application/models/sponsorhomepage_model.php
+++ application/models/sponsorhomepage_model.php
@@ -13,6 +13,27 @@ class sponsorhomepage_model extends Base_Model {
 		$results = $this->db->query('SELECT * from scholarships where isactive AND donorid = ' .$donorid); 
 		$results = $results->result_array();
 		
+		#attach the tags to the scholarships
+		$counter = 0;
+		while($counter < sizeof($results)) {
+			$tags = $this->db->query('SELECT x.description as reqtype, getreqvalue(requirementtypeid, requirement) as reqval
+				from scholarships
+				join scholarshiprequirements using (scholarshipid) 
+				join requirementtypes x using (requirementtypeid)
+				where scholarshipid = ' .$results[$counter]['scholarshipid']);
+			$tags = $tags->result_array();
+			$results[$counter]['tags'] = $tags;
+			
+			$counter++;
+		}
+		
+		// $counter = 0;
+		// while($counter < sizeof($results)) {
+			// print_r($results[$counter]['tags']);
+			// print_r('<br>');
+			// $counter++;
+		// }
+		
 		return $results;
 	}
 }
