b92d87c425608fc4f98a64d3a0e77a95d0ec7439
Adelen Victoria Po Festin <festinadelen@gmail.com>
2013-08-30 05:57:38 +0800

    changed flavour of words

--- application/views/viewsponsordetails_view.php
+++ application/views/viewsponsordetails_view.php
@@ -84,7 +84,7 @@
 					<br/><br/>
 					<?*/?>
 					<div class="callout border-callout">
-					<a href="<?=base_url('viewstudentdetails')?>/viewstudentinfo/<?=$indiv_feedback['studentid']?>"><?=$indiv_feedback['firstname'].' '.$indiv_feedback['lastname']?> (<?=$indiv_feedback['name']?>) </a> wrote on <?=$indiv_feedback['insertedon']?> for <?=$indiv_feedback['title']?>: <br/>
+					<a href="<?=base_url('viewstudentdetails')?>/viewstudentinfo/<?=$indiv_feedback['studentid']?>"><?=$indiv_feedback['firstname'].' '.$indiv_feedback['lastname']?> (<?=$indiv_feedback['name']?>) </a> wrote about <?=$indiv_feedback['title']?> on <?=$indiv_feedback['insertedon']?>: <br/>
 					<strong><?=$indiv_feedback['feedback']?></strong><br/>
 					<b class="border-notch notch"></b>
 					<b class="notch"></b>
