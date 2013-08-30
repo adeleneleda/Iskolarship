82b14c9ee82f0eed37e9230cbaf26b115c73cb97
danantonioreyes <danantonioreyes@gmail.com>
2013-08-30 21:44:30 +0800

    nalimutan ko yung tags para sa admin

--- application/models/xadminscholarship_model.php
+++ application/models/xadminscholarship_model.php
@@ -12,12 +12,25 @@ class xadminscholarship_model extends Base_Model {
 		
 		$results = $this->db->query('SELECT * from scholarships where isactive = false'); 
 		$results = $results->result_array();
+		#attach the tags to the scholarships
+		$counter = 0;
+		while($counter < sizeof($results)) {
+			$tags = $this->db->query('SELECT x.description as reqtype, getreqvalue(requirementtypeid, requirement) as reqval
+				from scholarships
+				join scholarshiprequirements using (scholarshipid) 
+				join requirementtypes x using (requirementtypeid)
+				where scholarshipid = ' .$results[$counter]['scholarshipid']);
+			$tags = $tags->result_array();
+			$results[$counter]['tags'] = $tags;
+			
+			$counter++;
+		}
 		
 		return $results;
 	}
 	
 	public function approveScholarship($scholarshipid) {
-		$query = 'UPDATE scholarships SET isactive=TRUE';
+		$query = 'UPDATE scholarships SET isactive=TRUE where scholarshipid = ' .$scholarshipid;
 		$this->db->query($query);
 	}
 }
